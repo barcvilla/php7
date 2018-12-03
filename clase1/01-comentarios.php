eca3d371ee032753f567c1f112c990c60b7e3dc6
Carlos Villanueva <ceva_19@hotmail.com>
2018-12-02 21:29:43 -0300

    Php 7 desde cero

---
+++ clase1/01-comentarios.php
@@ -0,0 +1,16 @@
+<?php
+    //comentario de linea
+
+    /*
+        comentario de
+        varias lineas
+    */
+
+    /**
+     * Comentario Dock Blocks de
+     * varias lineas con mayor formato
+     */
+
+     //Declaracion de variables
+     $edad = 25;
+?>
---
+++ clase1/02-aritmetica.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * var_dump() no retorna informacion sobre el valor de la operacion
+ */
+echo var_dump(6-2);
+
+/**
+ * Prioridad de los Operadores Aritmeticos
+ * 
+ * de Izquiera a derecha
+ * Multiplicacion
+ * Division
+ * Modulos
+ * Suma
+ * Resta
+ */
+?>
